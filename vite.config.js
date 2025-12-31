ac7d1528e14385f2c574aa1ccc59194e27dbc869
trevo7737 <mr.panha7737@gmail.com>
2025-12-31 15:09:08 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base : '/Clone-Apple2/',
   plugins: [react()],
-  base : "/clone-apple-/",
 })
